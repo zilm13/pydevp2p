47bfbb2113a1ab3b01db1187b4ee181c1ead0b4c
Fabio Utzig <utzig@utzig.org>
2016-07-11 08:42:10 -0300

    Absolute imports

--- devp2p/service.py
+++ devp2p/service.py
@@ -69,9 +69,9 @@ class WiredService(BaseService):
     wire_protocol = None
 
     def on_wire_protocol_start(self, proto):
-        from protocol import BaseProtocol
+        from .protocol import BaseProtocol
         assert isinstance(proto, BaseProtocol)
 
     def on_wire_protocol_stop(self, proto):
-        from protocol import BaseProtocol
+        from .protocol import BaseProtocol
         assert isinstance(proto, BaseProtocol)
