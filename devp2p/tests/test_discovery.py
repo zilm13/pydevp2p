9ae8d301527dca4b8b0459c2b6a6e77bb391c87c
Fabio Utzig <utzig@utzig.org>
2016-07-11 08:31:13 -0300

    Use encode_hex/decode_hex, strings as bytes

--- devp2p/tests/test_discovery.py
+++ devp2p/tests/test_discovery.py
@@ -2,6 +2,7 @@ from devp2p import discovery
 from devp2p import kademlia
 from devp2p import crypto
 from devp2p.app import BaseApp
+from rlp.utils import decode_hex, encode_hex
 import gevent
 import random
 
@@ -40,7 +41,7 @@ def test_address():
 
     assert len(b_a6[0]) == 16
     assert len(b_a4[0]) == 4
-    assert isinstance(b_a6[1], str)
+    assert isinstance(b_a6[1], bytes)
 
     host_a = Address(hostname, port)
     assert host_a.ip in ("127.0.0.1", "::1")
@@ -60,7 +61,7 @@ class NodeDiscoveryMock(object):
 
         config = dict(
             discovery=dict(),
-            node=dict(privkey_hex=crypto.sha3(seed).encode('hex')),
+            node=dict(privkey_hex=encode_hex(crypto.sha3(seed))),
             p2p=dict(listen_port=port),
         )
         config_discovery = config['discovery']
@@ -99,7 +100,7 @@ def test_packing():
     bob = NodeDiscoveryMock(host='127.0.0.1', port=1, seed='bob').protocol
 
     cmd_id = 3 # findnode
-    payload = ['a', ['b', 'c']]
+    payload = [b'a', [b'b', b'c']]
     message = alice.pack(cmd_id, payload)
 
     r_pubkey, r_cmd_id, r_payload, mdc = bob.unpack(message)
@@ -128,15 +129,15 @@ def test_ping_pong():
 
 eip8_packets = dict(
     # ping packet with version 4, additional list elements
-    ping1 = '''
+    ping1 = decode_hex(b'''
     e9614ccfd9fc3e74360018522d30e1419a143407ffcce748de3e22116b7e8dc92ff74788c0b6663a
     aa3d67d641936511c8f8d6ad8698b820a7cf9e1be7155e9a241f556658c55428ec0563514365799a
     4be2be5a685a80971ddcfa80cb422cdd0101ec04cb847f000001820cfa8215a8d790000000000000
     000000000000000000018208ae820d058443b9a3550102
-    '''.translate(None, ' \t\n').decode('hex'),
+    '''.replace(b' ', b'').replace(b'\n', b'')),
 
     # ping packet with version 555, additional list elements and additional random data
-    ping2 = '''
+    ping2 = decode_hex(b'''
     577be4349c4dd26768081f58de4c6f375a7a22f3f7adda654d1428637412c3d7fe917cadc56d4e5e
     7ffae1dbe3efffb9849feb71b262de37977e7c7a44e677295680e9e38ab26bee2fcbae207fba3ff3
     d74069a50b902a82c9903ed37cc993c50001f83e82022bd79020010db83c4d001500000000abcdef
@@ -145,30 +146,30 @@ eip8_packets = dict(
     3fa4090c408f6b4bc3701562c031041d4702971d102c9ab7fa5eed4cd6bab8f7af956f7d565ee191
     7084a95398b6a21eac920fe3dd1345ec0a7ef39367ee69ddf092cbfe5b93e5e568ebc491983c09c7
     6d922dc3
-    '''.translate(None, ' \t\n').decode('hex'),
+    '''.replace(b' ', b'').replace(b'\n', b'')),
 
     # pong packet with additional list elements and additional random data
-    pong = '''
+    pong = decode_hex(b'''
     09b2428d83348d27cdf7064ad9024f526cebc19e4958f0fdad87c15eb598dd61d08423e0bf66b206
     9869e1724125f820d851c136684082774f870e614d95a2855d000f05d1648b2d5945470bc187c2d2
     216fbe870f43ed0909009882e176a46b0102f846d79020010db885a308d313198a2e037073488208
     ae82823aa0fbc914b16819237dcd8801d7e53f69e9719adecb3cc0e790c57e91ca4461c9548443b9
     a355c6010203c2040506a0c969a58f6f9095004c0177a6b47f451530cab38966a25cca5cb58f0555
     42124e
-    '''.translate(None, ' \t\n').decode('hex'),
+    '''.replace(b' ', b'').replace(b'\n', b'')),
 
     # findnode packet with additional list elements and additional random data
-    findnode = '''
+    findnode = decode_hex(b'''
     c7c44041b9f7c7e41934417ebac9a8e1a4c6298f74553f2fcfdcae6ed6fe53163eb3d2b52e39fe91
     831b8a927bf4fc222c3902202027e5e9eb812195f95d20061ef5cd31d502e47ecb61183f74a504fe
     04c51e73df81f25c4d506b26db4517490103f84eb840ca634cae0d49acb401d8a4c6b6fe8c55b70d
     115bf400769cc1400f3258cd31387574077f301b421bc84df7266c44e9e6d569fc56be0081290476
     7bf5ccd1fc7f8443b9a35582999983999999280dc62cc8255c73471e0a61da0c89acdc0e035e260a
     dd7fc0c04ad9ebf3919644c91cb247affc82b69bd2ca235c71eab8e49737c937a2c396
-    '''.translate(None, ' \t\n').decode('hex'),
+    '''.replace(b' ', b'').replace(b'\n', b'')),
 
     # neighbours packet with additional list elements and additional random data
-    neighbours = '''
+    neighbours = decode_hex(b'''
     c679fc8fe0b8b12f06577f2e802d34f6fa257e6137a995f6f4cbfc9ee50ed3710faf6e66f932c4c8
     d81d64343f429651328758b47d3dbc02c4042f0fff6946a50f4a49037a72bb550f3a7872363a83e1
     b9ee6469856c24eb4ef80b7535bcf99c0004f9015bf90150f84d846321163782115c82115db84031
@@ -181,7 +182,7 @@ eip8_packets = dict(
     13198a2e037073488203e78203e8b8408dcab8618c3253b558d459da53bd8fa68935a719aff8b811
     197101a4b2b47dd2d47295286fc00cc081bb542d760717d1bdd6bec2c37cd72eca367d6dd3b9df73
     8443b9a355010203b525a138aa34383fec3d2719a0
-    '''.translate(None, ' \t\n').decode('hex'),
+    '''.replace(b' ', b'').replace(b'\n', b'')),
 )
 
 def test_eip8_packets():
@@ -195,7 +196,7 @@ def test_eip8_packets():
 def get_app(port, seed):
     config = dict(
         discovery=dict(),
-        node=dict(privkey_hex=crypto.sha3(seed).encode('hex')),
+        node=dict(privkey_hex=encode_hex(crypto.sha3(seed))),
         p2p=dict(listen_port=port),
     )
     config_discovery = config['discovery']
@@ -290,7 +291,7 @@ def main():
     print("this node is")
     proto = app.services.discovery.protocol.kademlia
     this_node = proto.this_node
-    print(this_node.pubkey.encode('hex'))
+    print(encode_hex(this_node.pubkey))
 
     # add external node
 
