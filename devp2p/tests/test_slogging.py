8483fbe8a57a81e21ee9cbf2ee1a648d2547c9bc
Fabio Utzig <utzig@utzig.org>
2016-07-10 21:53:01 -0300

    Fix StringIO import

--- devp2p/tests/test_slogging.py
+++ devp2p/tests/test_slogging.py
@@ -1,7 +1,10 @@
 import logging
 import unittest
 from devp2p import slogging
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 class LoggingTest(unittest.TestCase):
