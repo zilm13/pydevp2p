4f4aa5d42d946c06906d132756b60a096c8ed654
Fabio Utzig <utzig@utzig.org>
2016-07-17 22:50:04 -0300

    Add remove_chars, flake8 fixes

--- devp2p/utils.py
+++ devp2p/utils.py
@@ -1,7 +1,9 @@
 import struct
 import rlp
-from rlp.utils import encode_hex, decode_hex, str_to_bytes
+from rlp.utils import encode_hex, decode_hex, str_to_bytes, bytes_to_str
+from rlp.utils import safe_ord
 import collections
+import sys
 
 ienc = int_to_big_endian = rlp.sedes.big_endian_int.serialize
 
@@ -19,12 +21,14 @@ def int_to_big_endian4(integer):
 ienc4 = int_to_big_endian4
 
 
-node_uri_scheme = b'enode://'
+node_uri_scheme = 'enode://'
 
 
 def host_port_pubkey_from_uri(uri):
-    assert uri.startswith(node_uri_scheme) and b'@' in uri and b':' in uri, uri
-    pubkey_hex, ip_port = uri[len(node_uri_scheme):].split(b'@')
+    b_node_uri_scheme = str_to_bytes(node_uri_scheme)
+    assert uri.startswith(b_node_uri_scheme) and \
+        b'@' in uri and b':' in uri, uri
+    pubkey_hex, ip_port = uri[len(b_node_uri_scheme):].split(b'@')
     assert len(pubkey_hex) == 2 * 512 // 8
     ip, port = ip_port.split(b':')
     return ip, port, decode_hex(pubkey_hex)
@@ -32,9 +36,21 @@ def host_port_pubkey_from_uri(uri):
 
 def host_port_pubkey_to_uri(host, port, pubkey):
     assert len(pubkey) == 512 // 8
-    uri = '{}{}@{}:{}'.format(node_uri_scheme, encode_hex(pubkey),
-                              host, port)
+    uri = '{}{}@{}:{}'.format(node_uri_scheme,
+                              bytes_to_str(encode_hex(pubkey)),
+                              str(host), port)
     return str_to_bytes(uri)
+
+
+PY3 = sys.version_info[0] >= 3
+
+
+def remove_chars(s, chars):
+    if PY3:
+        d = {safe_ord(c): None for c in chars}
+        return s.translate(d)
+    else:
+        return s.translate(None, chars)
 
 
 # ###### config helpers ###############
@@ -70,11 +86,14 @@ COLOR_END = '\033[0m'
 colors = ['\033[9%dm' % i for i in range(0, 7)]
 colors += ['\033[4%dm' % i for i in range(1, 8)]
 
+
 def cstr(num, txt):
     return '%s%s%s' % (colors[num % len(colors)], txt, COLOR_END)
 
+
 def cprint(num, txt):
     print(cstr(num, txt))
+
 
 def phx(x):
     return encode_hex(x)[:8]
