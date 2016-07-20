a498a34bff5ec5e28c5bab3e19d4003f7958596f
Fabio Utzig <utzig@utzig.org>
2016-07-20 16:38:33 -0300

    Switch skipif back to xfail

--- devp2p/tests/test_full_app.py
+++ devp2p/tests/test_full_app.py
@@ -158,8 +158,8 @@ class ExampleServiceAppDisconnect(ExampleService):
 
 @pytest.mark.parametrize('num_nodes', [3, 6])
 class TestFullApp:
-    @pytest.mark.skipif(platform.sys.version_info[0] >= 3,
-                        reason="Unkown failure on Python 3. See ethereum/pydevp2p#37")
+    @pytest.mark.xfail(platform.sys.version_info[0] >= 3,
+                       reason="Unkown failure on Python 3. See ethereum/pydevp2p#37")
     @pytest.mark.timeout(60)
     def test_inc_counter_app(self, num_nodes):
         class TestDriver(object):
@@ -180,8 +180,8 @@ class TestFullApp:
         )
 
 
-@pytest.mark.skipif(platform.sys.version_info[0] >= 3,
-                    reason="Unkown failure on Python3. See ethereum/pydevp2p#37")
+@pytest.mark.xfail(platform.sys.version_info[0] >= 3,
+                   reason="Unkown failure on Python3. See ethereum/pydevp2p#37")
 @pytest.mark.timeout(20)
 def test_app_restart():
     """
